9993b7a51443e6f5a74bc9de84e29ce1e51c6075
AJ Kishawi <veonztwo@gmail.com>
2020-07-27 14:12:31 +0300

    Add files via upload

---
+++ src/sample/EmptyCell.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.scene.paint.Color;
+
+public class EmptyCell extends Cell{
+    public EmptyCell(int x, int y, int w, int h, Color c) {
+        super(x, y, w, h, c);
+    }
+}
